27ed54b64fe5efc485655e9b0e2ca362ffe08f05
vorobeychik <vorob.games@mail.ru>
2021-06-23 17:20:52 +0300

    fix: delete old imports

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import {World3d,IWorld3d} from "../../world3d/world3d";
-import {useScene} from "../../hooks/useScene";
 import {useDispatch} from "react-redux";
-import {setScene,settScene} from '../../redux/sceneSlice'
+
 
 export  function Scene(){
     const dispatch = useDispatch();
@@ -12,7 +11,7 @@ export  function Scene(){
     React.useLayoutEffect(() => {
         if (canvasRef.current) {
             world3d.current = new World3d(canvasRef.current);
-            dispatch(setScene(world3d.current))
+
         }
     }, []);
 
